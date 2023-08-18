7745be35fdca6e24c9ba9e75b2c48cd82c90a457
kkrupaa <kkrupaa@users.noreply.github.com>
2023-08-18 20:30:31 +0200

    Feeds - PriceMessage - Added some missing properties (still not all of them :( )

--- Source/FikaAmazonAPI/ConstructFeed/Messages/PriceMessage.cs
+++ Source/FikaAmazonAPI/ConstructFeed/Messages/PriceMessage.cs
@@ -1,14 +1,29 @@
-﻿namespace FikaAmazonAPI.ConstructFeed.Messages
+﻿using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace FikaAmazonAPI.ConstructFeed.Messages
 {
     public class PriceMessage
     {
         public string SKU { get; set; }
-
         public StandardPrice StandardPrice { get; set; }
-
-        public Sale Sale { get; set; }
-        public decimal BusinessPrice { get; set; }
+        public decimal? BusinessPrice { get; set; }
+        public QuantityPriceTypes? QuantityPriceType { get; set; }
+        [XmlIgnore]
+        public bool QuantityPriceTypeSpecified { get { return this.QuantityPriceType != null; } }
+        public QuantityPrice QuantityPrice { get; set; }
         public StandardPrice MinimumSellerAllowedPrice { get; set; }
         public StandardPrice MaximumSellerAllowedPrice { get; set; }
+        public StandardPrice MAP { get; set; }
+        public StandardPrice DepositAmount { get; set; }
+        public Sale Sale { get; set; }
+    }
+
+    public enum QuantityPriceTypes
+    {
+        [XmlEnum(Name = "fixed")]
+        Fixed,
+        [XmlEnum(Name = "percent")]
+        Percent
     }
 }
---
+++ Source/FikaAmazonAPI/ConstructFeed/Messages/QuantityPrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FikaAmazonAPI.ConstructFeed.Messages
+{
+    public class QuantityPrice
+    {
+        public decimal? QuantityPrice1 { get; set; }
+        public int? QuantityLowerBound1 { get; set; }
+        public decimal? QuantityPrice2 { get; set; }
+        public int? QuantityLowerBound2 { get; set; }
+        public decimal? QuantityPrice3 { get; set; }
+        public int? QuantityLowerBound3 { get; set; }
+        public decimal? QuantityPrice4 { get; set; }
+        public int? QuantityLowerBound4 { get; set; }
+        public decimal? QuantityPrice5 { get; set; }
+        public int? QuantityLowerBound5 { get; set; }
+    }
+   
+}
