652428ca8988cb44ea3ac1f6603f6fe5878a825a
kkrupaa <kkrupaa@users.noreply.github.com>
2024-02-07 12:11:22 +0100

    QuantityPrice - added ShouldSerialize* methods, to not serialize NULL values as Amazon rejects them.

--- Source/FikaAmazonAPI/ConstructFeed/Messages/QuantityPrice.cs
+++ Source/FikaAmazonAPI/ConstructFeed/Messages/QuantityPrice.cs
@@ -16,6 +16,46 @@ namespace FikaAmazonAPI.ConstructFeed.Messages
         public int? QuantityLowerBound4 { get; set; }
         public decimal? QuantityPrice5 { get; set; }
         public int? QuantityLowerBound5 { get; set; }
+        public bool ShouldSerializeQuantityPrice1()
+        {
+            return QuantityPrice1.HasValue;
+        }
+        public bool ShouldSerializeQuantityLowerBound1()
+        {
+            return QuantityLowerBound1.HasValue;
+        }
+        public bool ShouldSerializeQuantityPrice2()
+        {
+            return QuantityPrice2.HasValue;
+        }
+        public bool ShouldSerializeQuantityLowerBound2()
+        {
+            return QuantityLowerBound2.HasValue;
+        }
+        public bool ShouldSerializeQuantityPrice3()
+        {
+            return QuantityPrice3.HasValue;
+        }
+        public bool ShouldSerializeQuantityLowerBound3()
+        {
+            return QuantityLowerBound3.HasValue;
+        }
+        public bool ShouldSerializeQuantityPrice4()
+        {
+            return QuantityPrice4.HasValue;
+        }
+        public bool ShouldSerializeQuantityLowerBound4()
+        {
+            return QuantityLowerBound4.HasValue;
+        }
+        public bool ShouldSerializeQuantityPrice5()
+        {
+            return QuantityPrice5.HasValue;
+        }
+        public bool ShouldSerializeQuantityLowerBound5()
+        {
+            return QuantityLowerBound5.HasValue;
+        }
     }
    
 }
