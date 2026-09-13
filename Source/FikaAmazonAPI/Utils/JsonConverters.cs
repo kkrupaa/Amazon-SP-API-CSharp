d44d33d9838986d12654e06fb0116cc3271d8a54
pdamekwork@gmail.com <pdamekwork@gmail.com>
2026-09-13 13:58:12 +0200

    Transactions API fix

--- Source/FikaAmazonAPI/Utils/JsonConverters.cs
+++ Source/FikaAmazonAPI/Utils/JsonConverters.cs
@@ -1,6 +1,10 @@
-﻿namespace FikaAmazonAPI.Utils
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FikaAmazonAPI.Utils
 {
-    class Iso8601DateTimeConverter : Newtonsoft.Json.Converters.IsoDateTimeConverter
+    class Iso8601DateTimeConverter : IsoDateTimeConverter
     {
         public Iso8601DateTimeConverter()
         {
@@ -8,11 +12,33 @@
         }
     }
 
-    class Iso8601DateConverter : Newtonsoft.Json.Converters.IsoDateTimeConverter
+    class Iso8601DateConverter : IsoDateTimeConverter
     {
         public Iso8601DateConverter()
         {
             base.DateTimeFormat = "yyyy-MM-dd";
+        }
+    }
+
+    /// <summary>
+    /// Amazon occasionally returns undocumented enum values. Treat those as null
+    /// instead of failing deserialization of the entire response.
+    /// </summary>
+    public class StringEnumConverterIgnoreUnknown : StringEnumConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            try
+            {
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+            }
+            catch (JsonSerializationException)
+            {
+                if (Nullable.GetUnderlyingType(objectType) != null)
+                    return null;
+
+                return Activator.CreateInstance(objectType);
+            }
         }
     }
 }
